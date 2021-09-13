b8c05be28c1bc371c7584811c5cdfd6102026103
Daniel Werminghausen <daniel.werminghausen@gmail.com>
2021-09-13 12:42:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,26 +6,24 @@ var menuLinks = [
     { text: 'account', href: '/account' },
 ];
 // Part 1:
-// select and cache <main> into var
+// select and cache <main> into var mainEl
 let mainEl = document.querySelector('main');
 //style background color using 'var(--main-bg)';
 mainEl.style.backgroundColor = 'var(--main-bg)';
-// add h1
+// add content h1
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
-
-// object.classlist.add();
+// element.classlist.add();
 mainEl.classList.add('flex-ctr');
 
 ///////////////////////////////////////
 // Part 2:
-// get element by id 'top-menu'
-let topMenuEl = document.querySelector('#top-menu');
+// select and cache id top-menu into var topMenuEl
+let topMenuEl = document.getElementById('top-menu');
 // style the height 
 topMenuEl.style.height = '100%';
 //style background color using 'vvar(--top-menu-bg)';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-
-// object.classlist.add();
+// element.classlist.add();
 topMenuEl.classList.add('flex-around');
 
 
